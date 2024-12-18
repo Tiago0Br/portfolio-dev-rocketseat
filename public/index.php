8a24c94bf33d5dfb05143a53dc2f4126ee9a4ddf
Tiago Lopes <tiagoltavares2002@gmail.com>
2024-12-18 11:26:15 -0300

    chore: criação do projeto e configurações iniciais

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio Dev</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        red: '#E3646E',
+                        purple: '#BB72E9',
+                        blue: '#3996DB',
+                        green: '#82BC4F',
+                        yellow: '#EABD5F',
+                        'gray-100': '#0D0E11',
+                        'gray-200': '#16181D',
+                        'gray-300': '#292C34',
+                        'gray-400': '#878EA1',
+                        'gray-500': '#C0C4CE',
+                        'gray-600': '#E2E4E9'
+                    },
+                }
+            }
+        }
+    </script>
+</head>
+<body>
+    <h1 class="text-3xl font-bold text-red">
+        Portfólio dev
+    </h1>
+</body>
+</html>
